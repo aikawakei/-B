141d3759598afe8e38e57af1916b86cf8a9e7097
sho0719 <71868573+sho0719@users.noreply.github.com>
2022-06-25 12:03:14 +0900

    Add files via upload

---
+++ Sample.py
@@ -0,0 +1,18 @@
+import tkinter, tkinter.messagebox
+tki = tkinter.Tk()
+tki.geometry('300x200')
+tki.title('ラジオボタン')
+
+rdo_txt = ['Python','Java','C#']
+rdo_var = tkinter.IntVar()
+
+for i in range(len(rdo_txt)):
+    rdo = tkinter.Radiobutton(tki, value=i, variable=rdo_var, text=rdo_txt[i])
+    rdo.place(x=50, y=30 + (i * 24))
+
+    def btn_click():
+        num = rdo_var.get()
+        tkinter.messagebox.showinfo('チェックされた項目', rdo_txt[num])
+
+btn = tkinter.Button(tki, text='ラジオボタン取得', command=btn_click)
+btn.place(x=100, y=170) 

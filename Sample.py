f54cec161534a5aeb4f10234ed1b3af4f368bd7c
sho0719 <71868573+sho0719@users.noreply.github.com>
2022-06-25 11:32:43 +0900

    Update Sample.py

--- Sample.py
+++ Sample.py
@@ -1,3 +1,13 @@
-print('Sample')
-print('HTMLとにPythonを埋め込む練習')
-print('A-13')
+import tkinter
+			tki = tkinter.Tk()
+			tki.geometry('300x200')
+			tki.title('ラジオボタン')
+			
+			rdo1 = tkinter.Radiobutton(tki, text='サンプル問題1')
+			rdo1.place(x=70, y=40)
+			
+			rdo2 = tkinter.Radiobutton(tki, text='サンプル問題2')
+			rdo2.place(x=70, y=70)
+			
+			rdo3 = tkinter.Radiobutton(tki, text='サンプル問題3')
+			rdo3.place(x=70, y=100)

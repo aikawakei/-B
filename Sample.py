8f97fe0771aabbe51feab8da08250854c812c733
sho0719 <71868573+sho0719@users.noreply.github.com>
2022-06-25 13:48:04 +0900

    Update Sample.py

--- Sample.py
+++ Sample.py
@@ -9,10 +9,10 @@ rdo_var = tkinter.IntVar()
 for i in range(len(rdo_txt)):
     rdo = tkinter.Radiobutton(tki, value=i, variable=rdo_var, text=rdo_txt[i])
     rdo.place(x=50, y=30 + (i * 24))
-
+    
     def btn_click():
         num = rdo_var.get()
         tkinter.messagebox.showinfo('チェックされた項目', rdo_txt[num])
-
+        
 btn = tkinter.Button(tki, text='ラジオボタン取得', command=btn_click)
 btn.place(x=100, y=170) 
